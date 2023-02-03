e44e48048395f384a021edd602b4d90fc8457e00
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-02-03 11:49:57 +0700

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -2,29 +2,17 @@
 
 using namespace std;
 
-int main()
-{
-
+int main(){
     int longitude;
     cin >> longitude;
-    
- 
    int** array = (int**)malloc(longitude * (sizeof(int)));
-    
-     for (int i = 0; i < longitude; i++)
-        array[i] = (int*)malloc(longitude * (sizeof(int)));
-
     for (int i = 0; i < longitude; i++) {
+        array[i] = (int*)malloc(longitude * (sizeof(int)));
         for (int j = 0; j < longitude; j++) {
            array[i][j]=rand()%10;
+            cout << array[i][j] << '\t'; 
         }
+      cout << '\n';
+        free(array[i]);
     }
-    for (int i = 0; i < longitude; i++) {
-        for (int j = 0; j < longitude; j++) {
-            std::cout << array[i][j] << '\t';
-        }
-        std::cout << '\n';
-    }
-   
-  
 }
